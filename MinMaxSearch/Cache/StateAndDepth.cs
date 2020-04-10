da28687c98af23daf8c33c12437ba28ecb96fe05
ZviRosenfeld <zviRosenfeld2@gmail.com>
2020-04-10 16:38:38 +0300

    Added CacheKeyType

---
+++ MinMaxSearch/Cache/StateAndDepth.cs
@@ -0,0 +1,22 @@
+﻿namespace MinMaxSearch.Cache
+{
+    public class StateAndDepth
+    {
+        public StateAndDepth(IState state, int depth)
+        {
+            State = state;
+            Depth = depth;
+        }
+
+        public IState State { get; }
+
+        public int Depth { get; }
+
+        public override int GetHashCode() => State.GetHashCode() + Depth.GetHashCode();
+
+        public override bool Equals(object obj) =>
+            obj is StateAndDepth other &&
+            State.Equals(other.State) &&
+            Depth == other.Depth;
+    }
+}
---
+++ MinMaxSearch/Cache/StateAndPassedThroughStates.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MinMaxSearch.Cache
+{
+    class StateAndPassedThroughStates
+    {
+        public StateAndPassedThroughStates(IState state, IList<IState> passedThroughStates)
+        {
+            State = state;
+            PassedThroughStates = passedThroughStates;
+        }
+
+        public IState State { get; }
+
+        public IList<IState> PassedThroughStates { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is StateAndPassedThroughStates other))
+                return false;
+
+            if (!State.Equals(other.State))
+                return false;
+
+            if (PassedThroughStates.Count != other.PassedThroughStates.Count)
+                return false;
+
+            for (int i = 0; i < PassedThroughStates.Count; i++)
+                if (PassedThroughStates[i].Equals(other.PassedThroughStates[i]))
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode() => 
+            State.GetHashCode() + PassedThroughStates.Sum(passedThroughState => passedThroughState.GetHashCode());
+    }
+}
